9fb78a87862dc9385306744add2ff2e1310bbcd7
Jack <ahfhafh@gmail.com>
2023-01-22 01:22:09 -0700

    add new img

--- index.js
+++ index.js
@@ -32,3 +32,18 @@ blobTextEle.addEventListener("mouseleave", async () => {
     await new Promise(resolve => setTimeout(resolve));
     blob_open = false;
 });
+
+// var granimInstance = new Granim({
+//     element: '#bblob',
+//     direction: 'left-right',
+//     isPausedWhenNotInView: true,
+//     states: {
+//         "default-state": {
+//             gradients: [
+//                 ['#ff9966', '#ff5e62'],
+//                 ['#00F260', '#0575E6'],
+//                 ['#e1eec3', '#f05053']
+//             ]
+//         }
+//     }
+// });
