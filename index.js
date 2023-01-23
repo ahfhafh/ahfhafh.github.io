f9769d4888a628de4354b79edda3f108a7817a94
Jack <ahfhafh@gmail.com>
2023-01-22 18:43:33 -0700

    change blob container dynamically

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var blobEle = document.getElementById('blob');
+var blobbEle = document.getElementById('bblob');
 var blobTextEle = document.getElementById('blobText');
 var hiddenInfoEle = document.getElementById('hidden_info');
 
@@ -22,6 +23,7 @@ blobEle.addEventListener("mouseenter", () => {
         blob_open = true;
         hiddenInfoEle.style.display = "block";
         blobTextEle.style.pointerEvents = "auto";
+        blobbEle.style.height = "100%";
     }
 });
 
@@ -29,6 +31,7 @@ blobTextEle.addEventListener("mouseleave", async () => {
     info_to_blob.start();
     hiddenInfoEle.style.display = "none";
     blobTextEle.style.pointerEvents = "none";
-    await new Promise(resolve => setTimeout(resolve));
+    await new Promise(resolve => setTimeout(resolve, 750));
+    blobbEle.style.height = "230px";
     blob_open = false;
 });
